0d9bb8693ac5c39605f4f29a0cce6798b203ff7c
Gangashankar <bigdatamonk@gmail.com>
2019-11-08 17:59:26 +0530

    Train and Save Neural Network

--- RecogniseImages.py
+++
@@ -1,8 +0,0 @@
-import ImageLoader
-import NeuralNet
-
-training_data, validation_data, test_data = ImageLoader.load_data_wrapper()
-
-net = NeuralNet.Network([784, 300,20, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
-
